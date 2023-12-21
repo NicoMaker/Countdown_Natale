d1a9715c771db0358324ffe18e41d47e678a77b1
Nicola Marano <nicola.marano02@gmail.com>
2023-12-21 10:49:37 +0100

    First commit countdown

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let dataCorrente = new Date();
+    let annoCorrente = dataCorrente.getFullYear();
+
+    let countDownDate = new Date(`Dec 25, ${annoCorrente} 00:00:00`).getTime();
+    let x = setInterval(function () {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById('timer').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById('timer').innerHTML = 'MERY CHRISTMAS AND HAPPY NEY YEAR';
+        }
+    }, 1000);
+
+    let anno = `
+<p>&copy; ${annoCorrente} Il Mio Sito di Natale</p>`
+document.getElementById("anno").innerHTML=anno;
+});
